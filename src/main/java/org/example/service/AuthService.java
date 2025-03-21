0a31fea28f95c716f1f23156d8be8ce155f80392
QuangAnh1524 <quanganha6k58@gmail.com>
2025-03-21 15:38:05 +0700

    doneModel

--- src/main/java/org/example/service/AuthService.java
+++ src/main/java/org/example/service/AuthService.java
@@ -3,7 +3,7 @@ package org.example.service;
 import org.example.DAO.UserDAO;
 
 public class AuthService {
-    private boolean isLoggedIn = false;  // Trạng thái đăng nhập
+    private boolean isLoggedIn = false; 
 
     public boolean login(String username, String password) {
         UserDAO userDAO = new UserDAO();
@@ -14,13 +14,12 @@ public class AuthService {
         return isValid;
     }
 
-    public void logout() {
+    public boolean logout() {
         if (isLoggedIn) {
-            System.out.println("Bạn đã đăng xuất!");
             isLoggedIn = false;
-        } else {
-            System.out.println("Bạn chưa đăng nhập!");
+            return true;
         }
+        return false;
     }
 
     public boolean isAuthenticated() {
