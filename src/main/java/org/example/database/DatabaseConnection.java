efe8e0ee634e363c157af12ef46c5cb75df5b503
QuangAnh1524 <quanganha6k58@gmail.com>
2025-04-01 20:39:16 +0700

    pull

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/tutor";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456";
+    private static final String PASSWORD = "150204";
 
     public static Connection connection;
 
